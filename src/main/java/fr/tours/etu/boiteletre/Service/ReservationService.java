b725d0d166f949c8b29ca6351259c9d3f65d9a88
22210383t <radia.merabtene@etu.univ-tours.fr>
2025-10-10 09:31:56 +0200

    Implementing the Reservation and Utilisateur services

--- src/main/java/fr/tours/etu/boiteletre/Service/ReservationService.java
+++ src/main/java/fr/tours/etu/boiteletre/Service/ReservationService.java
@@ -1,4 +1,45 @@
 package fr.tours.etu.boiteletre.Service;
 
+import fr.tours.etu.boiteletre.Model.Boite;
+import fr.tours.etu.boiteletre.Model.Reservation;
+import fr.tours.etu.boiteletre.Model.Utilisateur;
+import fr.tours.etu.boiteletre.Repository.ReservationRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ReservationService {
+    private final ReservationRepository ReservationRepository;
+
+    public ReservationService(ReservationRepository reservationRepository) {
+        ReservationRepository = reservationRepository;
+    }
+
+    public Reservation getReservationById(int id) {
+        return ReservationRepository.getById(id);
+    }
+    public List<Reservation> getAllReservations() {
+        return ReservationRepository.findAll();
+    }
+    public Reservation createReservation(Reservation reservation) {
+        return ReservationRepository.save(reservation);
+    }
+    public Reservation createReservation(Boite boite, Utilisateur utilisateur) {
+        Reservation reservation = new Reservation(boite,utilisateur);
+        return ReservationRepository.save(reservation);
+    }
+    public void deleteReservation(int id) {
+        Reservation reservation = ReservationRepository.getById(id);
+        ReservationRepository.delete(reservation);
+    }
+    public Reservation updateReservation(Reservation reservation , int id) {
+        return ReservationRepository.findById(id).map(user ->{
+            user.setUtilisateurId(reservation.getUtilisateurId());
+            user.setBoite(reservation.getBoite());
+            return ReservationRepository.save(user);
+        }).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+
 }
--- src/main/java/fr/tours/etu/boiteletre/Service/UtilisateurService.java
+++ src/main/java/fr/tours/etu/boiteletre/Service/UtilisateurService.java
@@ -1,4 +1,52 @@
 package fr.tours.etu.boiteletre.Service;
 
+
+import fr.tours.etu.boiteletre.Model.Utilisateur;
+import fr.tours.etu.boiteletre.Repository.UtilisateurRepository;
+import jdk.jshell.execution.Util;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UtilisateurService {
+   private final UtilisateurRepository repo;
+
+
+    public UtilisateurService(UtilisateurRepository repo) {
+     this.repo = repo;
+
+    }
+public Utilisateur getById(int id) {
+     return repo.findById(id).orElse(null);
+}
+public List<Utilisateur> getAll() {
+     return repo.findAll();
+}
+public Utilisateur createUtilisateur(Utilisateur utilisateur) {
+     return repo.save(utilisateur);
+}
+public Utilisateur createUtilisateur(String nom , String prenom, String adresse, String email, String password,String UserName) {
+        Utilisateur utilisateur = new Utilisateur();
+        utilisateur.setNom(nom);
+        utilisateur.setPrenom(prenom);
+        utilisateur.setEmail(email);
+        utilisateur.setPassword(password);
+        utilisateur.setUsername(UserName);
+        return repo.save(utilisateur);
+}
+public void deleteUtilisateur(Utilisateur utilisateur) {
+     repo.delete(utilisateur);
+}
+public Utilisateur UpdateUtilisateur(Utilisateur utilisateur , int id) {
+    return repo.findById(id).map(user ->{
+        user.setNom(utilisateur.getNom());
+        user.setPrenom(utilisateur.getPrenom());
+        user.setEmail(utilisateur.getEmail());
+        user.setPassword(utilisateur.getPassword());
+        user.setUsername(utilisateur.getUsername());
+        return repo.save(user);
+    }).orElseThrow(() -> new RuntimeException("User not found"));
+}
+
 }
