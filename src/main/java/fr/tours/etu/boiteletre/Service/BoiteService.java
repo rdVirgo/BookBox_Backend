cb1dbac27be3cb8dddf6697d7c7b61922321d442
Mamadou Coulibaly <mamadou.coulibaly@etu.univ-tours.fr>
2025-10-10 09:31:50 +0200

    Creation of  BoiteService and CoordonneesService

--- src/main/java/fr/tours/etu/boiteletre/Service/BoiteService.java
+++ src/main/java/fr/tours/etu/boiteletre/Service/BoiteService.java
@@ -1,4 +1,43 @@
 package fr.tours.etu.boiteletre.Service;
 
+import fr.tours.etu.boiteletre.Model.Boite;
+import fr.tours.etu.boiteletre.Repository.BoiteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BoiteService {
+
+    private final BoiteRepository boiteRepository;
+
+    @Autowired
+    public BoiteService(BoiteRepository boiteRepository){
+        this.boiteRepository = boiteRepository;
+    }
+
+    public Boite addBoite(String name, int quantity, String description){
+        Boite boite = new Boite(name,quantity,description);
+        return boiteRepository.save(boite);
+    }
+
+    public void deleteBoiteById(int id){
+        boiteRepository.deleteById(id);
+    }
+
+    public List<Boite> getAllBoite(){
+        return boiteRepository.findAll();
+    }
+
+    public Boite updateBoite(int id, String name, int quantity, String description){
+        Boite boite = boiteRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("Boite not found !"));
+
+        boite.setNom(name);
+        boite.setQuantite(quantity);
+        boite.setDescription(description);
+
+        return boiteRepository.save(boite);
+    }
+
 }
--- src/main/java/fr/tours/etu/boiteletre/Service/CoordonneesService.java
+++ src/main/java/fr/tours/etu/boiteletre/Service/CoordonneesService.java
@@ -1,4 +1,48 @@
 package fr.tours.etu.boiteletre.Service;
 
+
+import fr.tours.etu.boiteletre.Model.Boite;
+import fr.tours.etu.boiteletre.Model.Coordonnees;
+import fr.tours.etu.boiteletre.Repository.CoordonneesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CoordonneesService {
+
+    private final CoordonneesRepository coordonneesRepository;
+
+    @Autowired
+    public CoordonneesService(CoordonneesRepository coordonneesRepository){
+        this.coordonneesRepository = coordonneesRepository;
+    }
+
+    public Coordonnees addCoordonnees(String latitude, String longitude){
+        Coordonnees coordonnees = new Coordonnees(latitude, longitude);
+        return coordonneesRepository.save(coordonnees);
+    }
+
+    public Coordonnees addCoordonnees(Coordonnees coordonnees){
+        return coordonneesRepository.save(coordonnees);
+    }
+
+    public void deleteCoordonneesById(int id){
+        coordonneesRepository.deleteById(id);
+    }
+
+    public List<Coordonnees> getAllCoordonnes(){
+        return coordonneesRepository.findAll();
+    }
+
+    public Coordonnees updateCoordonnees(int id, String latitude, String longitude){
+        Coordonnees boite = coordonneesRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("Boite not found !"));
+
+        boite.setLatitude(latitude);
+        boite.setLongitude(longitude);
+
+        return coordonneesRepository.save(boite);
+    }
+
 }
