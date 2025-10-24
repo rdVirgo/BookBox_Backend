da0706c49ac26ec4418bc4ff28fddc1532941e99
Mamadou Coulibaly <mamadou.coulibaly@etu.univ-tours.fr>
2025-10-24 17:37:26 +0200

    Box and Coordinates Dto.

--- src/main/java/fr/tours/etu/boiteletre/DTO/BoxDTO.java
+++ src/main/java/fr/tours/etu/boiteletre/DTO/BoxDTO.java
@@ -1,4 +1,41 @@
 package fr.tours.etu.boiteletre.DTO;
 
+import lombok.Data;
+
+import java.util.Objects;
+
+@Data
 public class BoxDTO {
+    private String name;
+    private int quantity;
+    private String description;
+
+    public BoxDTO(String name, int quantity, String description) {
+        this.name = name;
+        this.quantity = quantity;
+        this.description = description;
+    }
+
+    @Override
+    public String toString(){
+        return name + " " + quantity + " " + description;
+    }
+
+    @Override
+    public boolean equals(Object object){
+        if (this == object) return true;
+        if (object == null || this.getClass() != object.getClass()) return false;
+
+        BoxDTO boxDTO = (BoxDTO) object;
+
+        return Objects.equals(name,boxDTO.name) && Objects.equals(quantity,boxDTO.quantity) &&
+                Objects.equals(description,boxDTO.description);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hashCode(name) + Objects.hashCode(quantity) + Objects.hashCode(description);
+    }
+
+
 }
--- src/main/java/fr/tours/etu/boiteletre/DTO/CoordinatesDTO.java
+++ src/main/java/fr/tours/etu/boiteletre/DTO/CoordinatesDTO.java
@@ -1,4 +1,36 @@
 package fr.tours.etu.boiteletre.DTO;
 
+import lombok.Data;
+
+import java.util.Objects;
+
+@Data
 public class CoordinatesDTO {
+    private String latitude;
+    private String longitude;
+
+    public CoordinatesDTO(String latitude, String longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    @Override
+    public String toString(){
+        return latitude + " " + longitude;
+    }
+
+    @Override
+    public boolean equals(Object object){
+        if (this == object) return true;
+        if (object == null || this.getClass() != object.getClass()) return false;
+
+        CoordinatesDTO coordinatesDTO = (CoordinatesDTO) object;
+
+        return Objects.equals(latitude,coordinatesDTO.latitude) && Objects.equals(longitude,coordinatesDTO.longitude);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hashCode(latitude) + Objects.hashCode(longitude);
+    }
 }
