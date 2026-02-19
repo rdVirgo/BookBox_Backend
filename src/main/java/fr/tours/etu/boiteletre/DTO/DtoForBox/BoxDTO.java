2deb624357b2b4abedac13af7aa638d0f00ab03c
rdVirgo <studiesradia@gmail.com>
2026-02-19 19:04:14 +0100

    Update author in BoxDTO documentation

--- src/main/java/fr/tours/etu/boiteletre/DTO/DtoForBox/BoxDTO.java
+++ src/main/java/fr/tours/etu/boiteletre/DTO/DtoForBox/BoxDTO.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 /**
  *  A DTO class for the box entity
  *
- * @author Mamadou COULIBALY & Radia MERABTENE
+ * @author Mamadou COULIBALY & dvirgo
  * @version 1.0
  */
 
