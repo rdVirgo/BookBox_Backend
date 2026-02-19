2ef731f26fb2944dea01a68d446e3d4f15a1166f
rdVirgo <studiesradia@gmail.com>
2026-02-19 19:08:47 +0100

    updating doc

--- src/main/java/fr/tours/etu/boiteletre/MappStruct/UserMapper.java
+++ src/main/java/fr/tours/etu/boiteletre/MappStruct/UserMapper.java
@@ -7,7 +7,7 @@ import org.mapstruct.Mapper;
  *  A mapper for the user entity
  * convert a user to userDTO
  * covert a userDTO to a user
- * @author Mamadou COULIBALY & Radia MERABTENE
+ * @author Mamadou COULIBALY & rdvirgo
  * @version 1.0
  */
 @Mapper(componentModel = "spring")
